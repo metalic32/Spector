c1c2b74ad4dafcd85f711e702669ce780218e555
metalic32 <metalic@tlen.pl>
2015-07-12 20:29:14 +0200

    poprawki

--- Assets/Scripts/HeroManager.cs
+++ Assets/Scripts/HeroManager.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class HeroManager : MonoBehaviour {
 
 	public float walkSpeed = 1.0f;
+	public float runSpeed = 1.0f;
+	public bool lookingRight = true;
+	public Animator anim;
 
 	// Use this for initialization
 	void Start () {
@@ -12,9 +15,48 @@ public class HeroManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
-		float nextposition = this.transform.position.x  + walkSpeed * Time.deltaTime;
-		this.transform.position = new Vector3(nextposition,this.transform.position.y,this.transform.position.z);
+		float speed = walkSpeed;
+		if (Input.GetKey (KeyCode.D)) 
+		{
+			if (!lookingRight)
+				Flip();
+			if (Input.GetKey (KeyCode.LeftShift))
+			{
+				anim.SetInteger("State",2);
+				speed = runSpeed;
+			}
+			else anim.SetInteger("State",1);
+			float nextposition = this.transform.position.x  + speed * Time.deltaTime;
+			this.transform.position = new Vector3(nextposition,this.transform.position.y,this.transform.position.z);
 
+		}
+		else if (Input.GetKey (KeyCode.A)) 
+		{
+			if (lookingRight)
+				Flip();
+			if (Input.GetKey (KeyCode.LeftShift))
+			{
+				anim.SetInteger("State",2);
+				speed = runSpeed;
+			}
+			else anim.SetInteger("State",1);
+
+			float nextposition = this.transform.position.x  - speed * Time.deltaTime;
+			this.transform.position = new Vector3(nextposition,this.transform.position.y,this.transform.position.z);
+
+		}
+		else anim.SetInteger("State",0);
+
+		if (Input.GetKey (KeyCode.W)) 
+			anim.SetBool("Jump",true);
+		else anim.SetBool("Jump",false);
+
+	}
+	public void Flip()
+	{
+		lookingRight = !lookingRight;
+		Vector3 myScale = anim.transform.gameObject.transform.localScale;
+		myScale.x *= -1;
+		anim.transform.gameObject.transform.localScale = myScale;
 	}
 }
