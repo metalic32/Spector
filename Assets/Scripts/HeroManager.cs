390be072715d0d8084d798861ca88abc809632b3
metalic32 <metalic@tlen.pl>
2015-07-12 11:08:16 +0200

    1 commit

---
+++ Assets/Scripts/HeroManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeroManager : MonoBehaviour {
+
+	public float walkSpeed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		float nextposition = this.transform.position.x  + walkSpeed * Time.deltaTime;
+		this.transform.position = new Vector3(nextposition,this.transform.position.y,this.transform.position.z);
+
+	}
+}
